89a3e9b3d448240112c212c98110200f31a7a080
koreanair <32173060+koreanair@users.noreply.github.com>
2019-09-01 23:09:09 +0100

    -setcc (set byte on condition) added & passes testcase

--- debugger/Emulator/Opcodes/S/Set.cs
+++ debugger/Emulator/Opcodes/S/Set.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections.Generic;
-using debugger.Util;
+﻿using debugger.Util;
 
 namespace debugger.Emulator.Opcodes
 {
     public class Set : Opcode 
     {
-        byte[] Result;
-        public Set(DecodedTypes.IMyDecoded input, Condition setCondition, OpcodeSettings settings = OpcodeSettings.NONE) : base("hi", input, settings)
+        byte Result;
+        public Set(DecodedTypes.IMyDecoded input, Condition setCondition, OpcodeSettings settings = OpcodeSettings.NONE) 
+            : base("SET" + Disassembly.DisassembleCondition(setCondition), input, settings | OpcodeSettings.BYTEMODE)
         {
-            List<byte[]> DestSource = Fetch();
-            if (TestCondition(setCondition))
-            {
-          //    Result = Bitwise.ZeroExtend(new byte[] { 1 }, (int)Capacity);
-            }
+            Result = (byte)(TestCondition(setCondition) ? 1 : 0);
         }
         public override void Execute()
         {
-            Set(Result);
+            Set(Bitwise.ZeroExtend(new byte[] { Result }, (byte)Capacity));
         }
     }
 }
